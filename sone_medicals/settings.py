81c824d49f54766822e76b5d17c1296a9b7a0e56
md ehtesham <ehteshammd089@gmail.com>
2025-02-26 18:06:23 +0530

    frontend backend files

--- sone_medicals/settings.py
+++ sone_medicals/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'src',
 ]
 
 MIDDLEWARE = [
@@ -47,14 +48,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+
 ]
+CORS_ALLOW_ALL_ORIGINS = True  # Allow all frontend requests
 
 ROOT_URLCONF = 'sone_medicals.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
